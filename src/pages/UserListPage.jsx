3638006a8763307a39fe8750c8be62c9fa0098ae
kame-style <gouravad12@gmail.com>
2025-02-24 12:16:29 +0530

    final commit

--- src/pages/UserListPage.jsx
+++ src/pages/UserListPage.jsx
@@ -1,11 +1,14 @@
-import UserList from "../components/UserList";
-import React from "react";
+import { lazy, Suspense } from "react";
+
+const UserList = lazy(() => import("../components/UserList"));
 
 const UserListPage = () => {
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-4">User List</h1>
-      <UserList />
+      <Suspense fallback={<div>Loading Users...</div>}>
+        <UserList />
+      </Suspense>
     </div>
   );
 };
